05b5b9ba249c72fce369fe7ffd74f1ecfa520f2d
Dilafruz Shukurova <dilafruzshukurova36@gmail.com>
2024-04-24 11:27:21 +0500

    dan

---
+++ index.js
@@ -0,0 +1,9 @@
+let ism = prompt("String turdagi ma'lumot kiriting: ");
+let son = prompt("Number turdagi ma'lumot kiriting: ");
+let mashina = prompt("Boolean turdagi ma'lumot kiriting: ");
+
+console.log("Siz kiritgan " + ism + " so'zi " + typeof ism + " typega tegishli");
+console.log("Siz kiritgan " + son + " soni " + typeof son + " typega tegishli");
+console.log("Siz kiritgan " + mashina + " mashinani " + typeof mashina + " typega tegishli");
+
+
